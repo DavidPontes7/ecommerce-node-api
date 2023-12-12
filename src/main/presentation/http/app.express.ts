3211beab1b04351eb27cda0234268778798b5c74
David Pontes <pontesdejesusdavid@gmail.com>
2023-12-11 22:26:33 -0300

    aula do dia 11/12/2023

--- src/main/presentation/http/app.express.ts
+++ src/main/presentation/http/app.express.ts
@@ -1,18 +1,34 @@
-import express, { Application } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { apiv1Router } from "./rest/api.v1";
-import helmet from "helmet"
-import compression from "compression"
+import helmet from "helmet";
+import compression from 'compression';
 import { customMorganMiddleware } from "./middlewares/custom-morgan.middlewares";
-
+import { errorLogger } from "./middlewares/error-logger.middlewares";
+import { errorResponder } from "./middlewares/error-responser.middlewares";
+import { invalidPath } from "./middlewares/invalid-path.middlewares";
 const createExpressApplication = async (): Promise<Application>  => {
     const app: Application = express();
     app.disable('x-powered-by');
+
+    //Middlewares Integrados (Built-in)
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    //Middlewares de Terceiros
     app.use(helmet());
     app.use(compression());
-    app.use(express.urlencoded({ extended: true }));    
-    app.use(express.json());
+
+    //Middleware Customizados
     app.use(customMorganMiddleware);
+   
+    //Middlewares de Rotas
     app.use('/api/v1', apiv1Router);
+
+     //Middleware de Tratamento de Erros (Error Handling)
+    app.use(invalidPath);
+    app.use(errorLogger);
+    app.use(errorResponder);
+
     return app;
 };
 
